3f9ac8229111474cc0d8fab135c02a69e413a425
Jan Mrázek <email@honzamrazek.cz>
2018-07-10 17:42:13 +0200

    Use image previews

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var args = minimist(process.argv.slice(2));
 var ftp = require('vinyl-ftp');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
+var resizer = require('gulp-images-resizer');
 
 gulp.task('deploy', function() {
   var remotePath = '/';
@@ -13,12 +14,23 @@ gulp.task('deploy', function() {
     password: args.password,
     log: gulp.log
   });
-  gulp.src(['*.html', './img/**', 'css/*.css', 'fonts/*', 'js/*.js', 'favicon.ico'])
+  gulp.src(['*.html', './img/**', 'preview/**', 'css/*.css', 'fonts/*', 'js/*.js', 'favicon.ico'])
     .pipe(conn.newer(remotePath))
     .pipe(conn.dest(remotePath));
 });
 
-gulp.task('build', function() {
+gulp.task('img-preview', function() {
+  return gulp.src('img/**/*.*')
+      .pipe(resizer({
+          format: "jpg",
+          width: "30%",
+          quality: 70
+      }))
+  .pipe(gulp.dest('preview/'));
+});
 
+gulp.task('build', ['img-preview'], function() {
 
 });
+
+
