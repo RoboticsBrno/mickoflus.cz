a97b40c221cbb786317b4175885ee015f41baff1
Jan Mrázek <email@honzamrazek.cz>
2019-01-10 19:02:58 +0100

    Change deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,5 @@
 var gulp = require('gulp');
-var minimist = require('minimist');
-var args = minimist(process.argv.slice(2));
-var ftp = require('vinyl-ftp');
-var minimist = require('minimist');
-var args = minimist(process.argv.slice(2));
 var resizer = require('gulp-images-resizer');
-var log = require('fancy-log');
-
-gulp.task('deploy', function() {
-  var remotePath = '/';
-  var conn = ftp.create({
-    host: args.server,
-    user: args.user,
-    password: args.password,
-    log: gulp.log,
-    secureOptions: {rejectUnauthorized: false},
-    secure: true
-  });
-  return gulp.src(
-    ['*.html', 'img/**', 'preview/**', 'css/*.css',
-     'fonts/*', 'js/*.js', 'favicon.ico'],
-     {base: '.'})
-    .pipe(conn.newer(remotePath))
-    .on('data', (file) => {
-      log("Updating " + file.path);
-    })
-    .on('end', () => {
-      log("Deployed");
-    })
-    .pipe(conn.dest(remotePath));
-});
 
 gulp.task('img-preview', function() {
   return gulp.src(['img/**/*.jpg', 'img/**/*.png'])
